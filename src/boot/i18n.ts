87ca48a988786d1361caf41891039e58a6b307c7
rozx <lisida900710@gmail.com>
2025-12-25 22:17:37 -0500

    refactor: enhance type annotations in configuration and boot files for better TypeScript support
    - Updated type definitions in quasar.config.ts, axios.ts, i18n.ts, and primevue.ts to use 'any' for context parameters.
    - Introduced a new type declaration file for Quasar wrappers to improve type safety and clarity in the project.

--- src/boot/i18n.ts
+++ src/boot/i18n.ts
@@ -18,7 +18,7 @@ declare module 'vue-i18n' {
 }
 /* eslint-enable @typescript-eslint/no-empty-object-type */
 
-export default defineBoot(({ app }) => {
+export default defineBoot(({ app }: any) => {
   const i18n = createI18n<{ message: MessageSchema }, MessageLanguages>({
     locale: 'zh-CN',
     fallbackLocale: 'en-US',
